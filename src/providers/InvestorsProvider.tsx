edfabb735a691aaeb9abae76fa8b0dd0389c8483
sieugene <sieugene@mail.ru>
2021-06-18 10:53:31 +0300

    feature : change provider of pathname

---
+++ src/providers/InvestorsProvider.tsx
@@ -0,0 +1,25 @@
+import { ModalProvider } from '@alium-official/uikit'
+import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { NetworkContextName } from '../constants'
+import store from '../state'
+import { ThemeContextProvider } from '../ThemeContext'
+import getLibrary from '../utils/getLibrary'
+
+const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
+
+const InvestorsProvider: React.FC = ({ children }) => {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Web3ProviderNetwork getLibrary={getLibrary}>
+        <Provider store={store}>
+          <ThemeContextProvider>
+            <ModalProvider>{children}</ModalProvider>
+          </ThemeContextProvider>
+        </Provider>
+      </Web3ProviderNetwork>
+    </Web3ReactProvider>
+  )
+}
+export default InvestorsProvider
---
+++ src/providers/MainProvider.tsx
@@ -0,0 +1,41 @@
+import { ModalProvider } from '@alium-official/uikit'
+import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core'
+import { BlockContextProvider } from 'contexts/BlockContext'
+import { LanguageContextProvider } from 'contexts/Localisation/languageContext'
+import { RefreshContextProvider } from 'contexts/RefreshContext'
+import { ThemeContextProvider } from 'contexts/ThemeContext'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { IntercomProvider } from 'react-use-intercom'
+import store from 'state'
+import { getLibrary } from 'utils/web3React'
+
+const Web3ProviderNetwork = createWeb3ReactRoot('NETWORK')
+
+const MainProvider: React.FC = ({ children }) => {
+  return (
+    <IntercomProvider
+      appId={process.env.REACT_APP_INTERCOM_APP_ID}
+      autoBoot
+      shouldInitialize={!!process.env.REACT_APP_INTERCOM_APP_ID}
+    >
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Web3ProviderNetwork getLibrary={getLibrary}>
+          <Provider store={store}>
+            <ThemeContextProvider>
+              <LanguageContextProvider>
+                <BlockContextProvider>
+                  <RefreshContextProvider>
+                    <ModalProvider>{children}</ModalProvider>
+                  </RefreshContextProvider>
+                </BlockContextProvider>
+              </LanguageContextProvider>
+            </ThemeContextProvider>
+          </Provider>
+        </Web3ProviderNetwork>
+      </Web3ReactProvider>
+    </IntercomProvider>
+  )
+}
+
+export default MainProvider
