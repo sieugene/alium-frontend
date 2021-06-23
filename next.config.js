fdd6fa13b98b628dd60112281b25eaa67727b465
Denis Vybornyj <denis.vybornyj@gmail.com>
2021-06-23 13:31:53 +0300

    chore: improve project settings

--- next.config.js
+++ next.config.js
@@ -1,15 +1,24 @@
 const webpack = require('webpack')
-const isDev = process.env.NODE_ENV === "development"
-const { parsed: myEnv } = isDev ? require('dotenv').config({
-    path:'./.env.development'
-}) : require('dotenv').config({
-    path:'./.env.production'
-})
-console.log('APP was started with MODE', isDev ? "DEV" : "PRODUCTION");
+const isDev = process.env.NODE_ENV === 'development'
+const { parsed: myEnv } = isDev
+  ? require('dotenv').config({
+      path: './.env.development',
+    })
+  : require('dotenv').config({
+      path: './.env.production',
+    })
+console.log('APP was started with MODE', isDev ? 'DEV' : 'PRODUCTION')
 
 module.exports = {
-    webpack(config) {
-        config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
-        return config
-    }
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
+    return config
+  },
+  distDir: 'build',
+  compress: false,
+  poweredByHeader: false,
+  devIndicators: {
+    autoPrerender: false,
+  },
+  reactStrictMode: true,
 }
