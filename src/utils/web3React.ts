5fbf2dc01f69f2cfb571edfec6939ebc85532c14
sieugene <sieugene@mail.ru>
2021-07-01 13:20:45 +0300

    fix : notify, validate supported chainId

--- src/utils/web3React.ts
+++ src/utils/web3React.ts
@@ -2,6 +2,7 @@ import { BscConnector } from '@binance-chain/bsc-connector'
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { ConnectorNames, getChainId } from 'alium-uikit/src'
+import { networksDev, networksProd } from '../alium-uikit/src/widgets/WalletModal/config'
 // import Web3 from 'web3'
 import getNodeUrl from './getRpcUrl'
 
@@ -10,16 +11,22 @@ export const getConnectorsByName = (connectorID: ConnectorNames) => {
   const chainId = getChainId()
   const rpcUrl = getNodeUrl(chainId)
 
-  const injected = new InjectedConnector({ supportedChainIds: [chainId] })
+  const isDev = process.env.NODE_ENV === 'development'
+  const networks = isDev ? networksDev : networksProd
+
+  const supported = networks.find((network) => network.chainId === chainId)
+  const supportedId = supported?.chainId
+
+  const injected = new InjectedConnector({ supportedChainIds: [supportedId] })
 
   const walletconnect = new WalletConnectConnector({
-    rpc: { [chainId]: rpcUrl as string },
+    rpc: { [supportedId]: rpcUrl as string },
     bridge: 'https://bridge.walletconnect.org',
     qrcode: true,
     pollingInterval: POLLING_INTERVAL,
   })
 
-  const bscConnector = new BscConnector({ supportedChainIds: [chainId] })
+  const bscConnector = new BscConnector({ supportedChainIds: [supportedId] })
 
   const connectorsByName: { [connectorName in ConnectorNames]: any } = {
     [ConnectorNames.Injected]: injected,
@@ -27,7 +34,7 @@ export const getConnectorsByName = (connectorID: ConnectorNames) => {
     [ConnectorNames.BSC]: bscConnector,
   }
 
-  return { chainId, connector: connectorsByName[connectorID] }
+  return { chainId: supportedId, connector: connectorsByName[connectorID] }
 }
 
 export const getLibrary = (provider: any) => {
