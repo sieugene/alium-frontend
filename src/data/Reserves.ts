0c370c51709908ccf12b27672f10a5df4f66f457
Denis Vybornyj <denis.vybornyj@gmail.com>
2021-07-07 08:51:11 +0300

    fix: json import

--- src/data/Reserves.ts
+++ src/data/Reserves.ts
@@ -1,12 +1,12 @@
 import { Currency, Pair, TokenAmount } from '@alium-official/sdk'
 import { Interface } from '@ethersproject/abi'
-import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/IUniswapV2Pair.json'
+import IUniswapV2PairABI from '@uniswap/v2-core/build/IUniswapV2Pair.json'
 import { useMemo } from 'react'
 import { useActiveWeb3React } from '../hooks'
 import { useMultipleContractSingleData } from '../state/multicall/hooks'
 import { wrappedCurrency } from '../utils/wrappedCurrency'
 
-const PAIR_INTERFACE = new Interface(IUniswapV2PairABI)
+const PAIR_INTERFACE = new Interface(IUniswapV2PairABI.abi)
 
 export enum PairState {
   LOADING,
