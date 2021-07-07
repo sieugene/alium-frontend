c2aed476656164f9f5c847fce1504387b6f382ee
Denis Vybornyj <denis.vybornyj@gmail.com>
2021-07-07 08:53:09 +0300

    chore: update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     },
   },
   rules: {
+    'max-params': 0, // temp
     '@next/next/no-img-element': 0, // temp
     'react-hooks/rules-of-hooks': 2, // enable for errors: rules of hooks
     'react-hooks/exhaustive-deps': 1, // enable for warn: effect dependencies
