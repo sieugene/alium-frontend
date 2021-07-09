6edf9521090ca39ac031789dc19510784c13463f
sieugene <sieugene@mail.ru>
2021-07-09 17:24:26 +0300

    chore : format balance display

--- src/utils/currency/toSignificantCurrency.ts
+++ src/utils/currency/toSignificantCurrency.ts
@@ -1,16 +1,49 @@
 import { CurrencyAmount, Price } from '@alium-official/sdk'
+import BigNumber from 'bignumber.js'
+import { getBalanceNumber } from './../formatBalance'
+
 // toSignificant like metamask
-// export const toSignificantCurrency = (currency: CurrencyAmount | Price) => {
-//   if (currency && currency?.toSignificant) {
-//     const total = Number(currency.toSignificant(1000))
-//     const replaced = getFlooredFixed(total, 3)
-//     return formatZero(replaced, total.toString())
-//   }
-//   return ''
-// }
+
 export const toSignificantCurrency = (currency: CurrencyAmount | Price) => {
-  return currency?.toSignificant(6)
+  if (currency?.raw) {
+    const RawBN = new BigNumber(Number(`${currency.raw}`))
+    const balance = getBalanceNumber(RawBN)
+    return formatDigits(balance)
+    // old method
+    // return currency?.toSignificant(6)
+  }
+  return ''
 }
+const formatDigits = (balance: number) => {
+  const text = balance.toString()
+  const splitted = text.split('.')
+  const half = splitted?.length > 1 && splitted[1].split('')
+  const firstIsZero = half?.length >= 2 && half[0] + half[1] + half[2] === '000'
+  if (firstIsZero) {
+    return NonZero(half)
+  } else {
+    const replaced = getFlooredFixed(Number(balance), 3)
+    return formatZero(replaced, balance.toString())
+  }
+}
+// Оставляет нули, заполняя последним отличным от нуля
+const NonZero = (words: string[]) => {
+  let word = ''
+  let breaked = false
+  words.forEach((symbol) => {
+    const asNumber = Number(symbol)
+    if (!breaked) {
+      if (asNumber === 0) {
+        word += symbol
+      } else {
+        word += symbol
+        breaked = true
+      }
+    }
+  })
+  return word
+}
+
 function getFlooredFixed(v: number, d: number) {
   return (Math.floor(v * Math.pow(10, d)) / Math.pow(10, d)).toFixed(d)
 }
