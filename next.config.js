060dab1812bcce490699fa61de17686e6f4dc730
Denis Vybornyj <denis.vybornyj@gmail.com>
2021-07-27 18:05:33 +0300

    build: fix docker config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const { i18n } = require('./next-i18next.config')
 const APP_ENV = process.env.APP_ENV === 'development' ? 'development' : 'production'
-const env = require(APP_ENV === 'development' ? './src/.env.development.js' : './src/.env.production.js')
+const env = require(APP_ENV === 'development' ? './.env.development.js' : './.env.production.js')
 
 module.exports = {
   i18n,
