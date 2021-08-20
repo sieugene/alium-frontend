b5bcf289a6b2566b4d2602292cafbed89dfc7e39
sieugene <sieugene@mail.ru>
2021-08-20 17:03:29 +0300

    chore : remove liqudity add loader, fix failure when change networks on pool page

--- src/state/user/hooks.tsx
+++ src/state/user/hooks.tsx
@@ -246,6 +246,9 @@ export function useTrackedTokenPairs(): [Token, Token][] {
   return useMemo(() => {
     // dedupes pairs of tokens in the combined list
     const keyed = combinedList.reduce<{ [key: string]: [Token, Token] }>((memo, [tokenA, tokenB]) => {
+      if (tokenA?.chainId !== chainId || tokenB?.chainId !== chainId) {
+        return memo
+      }
       const sorted = tokenA?.sortsBefore(tokenB)
       const key = sorted ? `${tokenA.address}:${tokenB.address}` : `${tokenB.address}:${tokenA.address}`
       if (memo[key]) return memo
