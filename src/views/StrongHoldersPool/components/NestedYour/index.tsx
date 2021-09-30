182c0c63d3563c86a1f7aec8e4dfb8a70f81028c
Nikita Korshunov <kewka96@gmail.com>
2021-09-30 19:03:47 +0500

    fix(shp): handle an unconnected wallet

--- src/views/StrongHoldersPool/components/NestedYour/index.tsx
+++ src/views/StrongHoldersPool/components/NestedYour/index.tsx
@@ -1,3 +1,4 @@
+import { useWeb3React } from '@web3-react/core'
 import { Skeleton } from 'alium-uikit/src'
 import PaginateWithMore from 'components/PaginateWithMore'
 import { usePaginate } from 'components/Pagination/hooks/usePaginate'
@@ -11,11 +12,12 @@ const PAGE_LIMIT = 6
 
 export default function NestedYour() {
   const { data } = useYourPools()
+  const { account } = useWeb3React()
   const { items, ...paginate } = usePaginate({
     items: data || [],
     pageLimit: PAGE_LIMIT,
   })
-  return (
+  return account ? (
     <NestedYour.Root>
       <NestedYour.Pools>
         {data && items
@@ -24,7 +26,7 @@ export default function NestedYour() {
       </NestedYour.Pools>
       <PaginateWithMore {...paginate} />
     </NestedYour.Root>
-  )
+  ) : null
 }
 
 NestedYour.Pools = styled.div`
