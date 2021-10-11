8c31cb8d11f306f1a5c206c9b03ce9e7d377aaf8
Nikita Korshunov <kewka96@gmail.com>
2021-10-11 18:28:20 +0500

    fix(farms): gas options

--- src/utils/farm/calls/index.ts
+++ src/utils/farm/calls/index.ts
@@ -1,60 +1,30 @@
 import { TransactionResponse } from '@ethersproject/abstract-signer/node_modules/@ethersproject/abstract-provider'
 import BigNumber from 'bignumber.js'
-import { DEFAULT_GAS_LIMIT, DEFAULT_TOKEN_DECIMAL } from 'config'
 import { Contract } from 'ethers'
-import { calculateGasPrice } from 'utils'
 import { toWei } from 'utils/bigNumber'
 
-const options = {
-  gasLimit: DEFAULT_GAS_LIMIT,
-}
-
 export const stakeFarm = async (masterChefContract: Contract, pid: number, amount) => {
-  const value = new BigNumber(amount || '0').times(DEFAULT_TOKEN_DECIMAL).toString()
-  const gasPrice = await calculateGasPrice(masterChefContract.provider)
-
-  // if (pid === 0) {
-  //   const tx = await masterChefContract.stake(value, { gasPrice })
-  //   const receipt = await tx.wait()
-  //   return receipt.status
-  // }
-
-  const tx: TransactionResponse = await masterChefContract.deposit(pid, value, { gasPrice })
-
+  const tx: TransactionResponse = await masterChefContract.deposit(pid, toWei(new BigNumber(amount || '0')).toString())
   const receipt = await tx.wait()
-
   if (!receipt.status) {
     throw new Error('transaction was failed')
   }
-
   return receipt?.transactionHash
 }
 
 export const unstakeFarm = async (masterChefContract: Contract, pid, amount) => {
   await harvestFarm(masterChefContract, pid)
-  const tx = await masterChefContract.withdraw(pid, toWei(new BigNumber(amount)).toString(), {
-    gasPrice: calculateGasPrice(masterChefContract.provider),
-  })
+  const tx = await masterChefContract.withdraw(pid, toWei(new BigNumber(amount)).toString())
   const receipt = await tx.wait()
-
   if (!receipt.status) {
     throw new Error('transaction was failed')
   }
-
   return receipt.status
 }
 
 export const harvestFarm = async (masterChefContract: Contract, pid: number) => {
-  const gasPrice = calculateGasPrice(masterChefContract.provider)
-  // if (pid === 0) {
-  //   const tx = await masterChefContract.unstake('0', { gasPrice })
-  //   const receipt = await tx.wait()
-  //   return receipt.status
-  // }
-
-  const tx: TransactionResponse = await masterChefContract.deposit(pid, '0', { gasPrice })
+  const tx: TransactionResponse = await masterChefContract.deposit(pid, '0')
   const receipt = await tx.wait()
-
   if (!receipt.status) {
     throw new Error('transaction was failed')
   }
