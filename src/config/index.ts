c15a16462a00eed9b94ddf47d1d9e97c67ffacc3
Nikita Korshunov <kewka96@gmail.com>
2021-10-13 10:37:31 +0500

    chore: config/bignumber

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,5 @@
 import BigNumber from 'bignumber.js'
 
-export const BNConfig = BigNumber.config({
-  EXPONENTIAL_AT: 1000,
-  DECIMAL_PLACES: 80,
-})
-
 export const ALM_PER_BLOCK = new BigNumber(7)
 // export const ALM_PER_BLOCK = new BigNumber(40)
 export const BSC_BLOCK_TIME = 3
